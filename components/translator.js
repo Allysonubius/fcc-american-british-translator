c24cf625f4b2584622b636469de69979f7ffe27b
Anthony Suárez <neolight1010@gmail.com>
2021-03-31 18:16:18 -0500

    Format with Prettier

--- components/translator.js
+++ components/translator.js
@@ -1,22 +1,30 @@
-const utils = require('./utils');
+const utils = require("./utils");
 
 const americanOnly = require("./american-only.js");
 const americanToBritishSpelling = require("./american-to-british-spelling.js");
 const americanToBritishTitles = require("./american-to-british-titles.js");
 const britishOnly = require("./british-only.js");
 
-function smartReplace(sentence, dictionary, highlight = (s) => s, ignorePunctuation=true) {
+function smartReplace(
+  sentence,
+  dictionary,
+  highlight = (s) => s,
+  ignorePunctuation = true
+) {
   const _punctuation = /[.,]/g;
   let newSentence = sentence.slice();
 
-  const words = sentence.split(' ');
+  const words = sentence.split(" ");
   const nWords = words.length;
   let phrases = []; // Phrases of l length found in sentence.
 
   for (let l = nWords; l > 0; l--) {
     for (let i = 0; i < nWords - l + 1; i++) {
-      let newPhrase = words.slice(i, i + l).join(' ').toLowerCase();
-      if (ignorePunctuation) newPhrase = newPhrase.replace(_punctuation, '');
+      let newPhrase = words
+        .slice(i, i + l)
+        .join(" ")
+        .toLowerCase();
+      if (ignorePunctuation) newPhrase = newPhrase.replace(_punctuation, "");
       phrases.push(newPhrase);
     }
   }
@@ -24,12 +32,13 @@ function smartReplace(sentence, dictionary, highlight = (s) => s, ignorePunctuat
   // Replace
   phrases.forEach((phrase) => {
     // console.log(`\n[${phrase}]: [${dictionary[phrase]}]`);
-    let regexp = new RegExp(phrase, 'gi');
-    if (dictionary[phrase]) newSentence = newSentence.replace(regexp, highlight(dictionary[phrase]));
+    let regexp = new RegExp(phrase, "gi");
+    if (dictionary[phrase])
+      newSentence = newSentence.replace(regexp, highlight(dictionary[phrase]));
   });
 
   return newSentence;
-};
+}
 
 function highlight(word) {
   return '<span class="highlight">' + word + "</span>";
@@ -41,18 +50,25 @@ function highlightCapitalize(word) {
 }
 
 function americanToBritishTime(sentence, highlight) {
-  return utils.replaceTimeSeparator(sentence, ':', '.', highlight); 
+  return utils.replaceTimeSeparator(sentence, ":", ".", highlight);
 }
 
 class Translator {
-  
-
   americanToBritish(sentence) {
     let newSentence = sentence.slice();
 
     newSentence = smartReplace(newSentence, americanOnly, highlight);
-    newSentence = smartReplace(newSentence, americanToBritishSpelling, highlight);
-    newSentence = smartReplace(newSentence, americanToBritishTitles, highlightCapitalize, false);
+    newSentence = smartReplace(
+      newSentence,
+      americanToBritishSpelling,
+      highlight
+    );
+    newSentence = smartReplace(
+      newSentence,
+      americanToBritishTitles,
+      highlightCapitalize,
+      false
+    );
     newSentence = americanToBritishTime(newSentence, highlight);
 
     return newSentence;
